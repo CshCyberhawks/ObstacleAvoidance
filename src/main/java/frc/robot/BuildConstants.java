0a1c961e0d5f5fc11dbdbdc327a91504d99ab99e
Can Kurç <cankurc@cans-mbp.devices.brown.edu>
2023-07-13 14:25:44 -0400

    robot simulation video added

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ObstacleAvoidance";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "6d58908474b9c4c51d1b3d8e0abf0725c0819afb";
+  public static final String GIT_DATE = "2023-07-11 14:29:23 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-07-11 16:57:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1689109070582L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
